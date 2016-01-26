3cf3556832f4e4c906ad392ffff9baea9f9e6d22
Juan Crisóstomo <juancvfenix@gmail.com>
2016-01-26 16:58:20 -0600

    #1, modificando recursos de plantilla

--- web-app/js/controllers/UISelectController.js
+++ web-app/js/controllers/UISelectController.js
@@ -5,7 +5,7 @@
  * We want to perform a OR.
  */
 
-angular.module('MetronicApp').filter('propsFilter', function() {
+angular.module('CapitalBusApp').filter('propsFilter', function() {
     return function(items, props) {
         var out = [];
 
@@ -36,7 +36,7 @@ angular.module('MetronicApp').filter('propsFilter', function() {
     };
 });
 
-angular.module('MetronicApp').controller('UISelectController', function($scope, $http, $timeout) {
+angular.module('CapitalBusApp').controller('UISelectController', function($scope, $http, $timeout) {
     $scope.$on('$viewContentLoaded', function() {
         App.initAjax(); // initialize core components
     });
